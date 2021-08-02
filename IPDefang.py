e97b0faa0fd5b8839a2a193e343028a70328ab6e
Lemon Zest <k.byrne357@gmail.com>
2021-08-02 21:47:08 +0100

    defanged

--- IPDefang.py
+++ IPDefang.py
@@ -4,3 +4,13 @@
 userInput = input("Enter IP address to be defanged: ")
 
 defangedIP = []
+
+for x in userInput:
+    if x == '.':
+        defangedIP.append("[.]")
+    else:
+        defangedIP.append(x)
+
+removeList =' '.join([str(i) for i in defangedIP])
+print (removeList)
+
